6b00a92a49763fb5a57ca6d3b92c4c92981fdf1b
prio12 <maksudurprio.mp@gmail.com>
2025-03-07 23:02:42 +0600

    notifications Schema added

---
+++ models/notificationsModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const notificationsSchema = new Schema({
+  type: {
+    type: String,
+    enum: ['like', 'comment', 'friend_request', 'friend_accept', 'message'],
+    required: true,
+  },
+  recipient: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+  },
+  comment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+  },
+  //   message: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'Message'
+  //   },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Notification = mongoose.model('Notification', notificationsSchema);
+module.exports = Notification;
