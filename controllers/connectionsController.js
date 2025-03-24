717f9db786d098a5c936adc265a122bc867000db
prio12 <maksudurprio.mp@gmail.com>
2025-03-24 18:35:17 +0600

    populating requester info in connection Requests

--- controllers/connectionsController.js
+++ controllers/connectionsController.js
@@ -46,7 +46,22 @@ async function createConnectionRequest(req, res) {
 
 //get connection request of a specific user
 async function getConnectionRequests(req, res) {
-  console.log(req.params.id);
+  const id = req.params.id;
+
+  try {
+    const response = await Connection.find({ recipient: id }).populate(
+      'requester'
+    );
+    res.status(200).json({
+      success: true,
+      response,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: 'Server Side Error!',
+    });
+  }
 }
 
 module.exports = {
