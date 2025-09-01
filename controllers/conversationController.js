6d662a1e62bc448a0e826b8a6c8c4a73e76743dc
prio12 <maksudurprio.mp@gmail.com>
2025-09-01 17:08:26 +0600

    successfully saving conversation and message in db also sending response to the client side

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -4,39 +4,29 @@ const Message = require('../models/messageModel');
 //create a new conversation
 async function createConversation(req, res) {
   const { participants, sender, text } = req.body;
-
-  console.log(text);
-
   if (!(participants && participants.length > 1) || !sender || !text) {
     return res.status(400).json({
       error: 'Minimum two participants, sender and text required',
     });
   }
-
   try {
     //looking for the conversation id of two certain participants
     let conversation = await Conversation.findOne({
       participants: { $all: participants },
     });
-
     if (!conversation) {
       conversation = new Conversation({ participants });
       await conversation.save();
     }
-
     //making new message
     const newMessage = new Message({
       conversation: conversation?._id,
       sender: '680fc98389d43fd618647bc6',
       text,
     });
-
     await newMessage.save();
-
     conversation.lastMessage = newMessage._id;
-
     await conversation.save();
-
     res.status(200).json({
       success: true,
       newMessage,
